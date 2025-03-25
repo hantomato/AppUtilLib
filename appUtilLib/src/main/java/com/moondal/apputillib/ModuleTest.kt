49f100b5e97578a6e88fa566cbc34bd65bf9ea78
neowiztomato <neowiztomato@neowiz.com>
2025-03-25 13:57:25 +0900

    first commit

---
+++ appUtilLib/src/main/java/com/moondal/apputillib/ModuleTest.kt
@@ -0,0 +1,18 @@
+package com.moondal.apputillib
+
+import android.util.Log
+
+/**
+ *
+ * Created by neowiztomato on 2025. 3. 25..
+ */
+class ModuleTest {
+    companion object {
+
+        @JvmStatic
+        fun testLog() {
+            Log.d("apputil", "apputil test log")
+        }
+
+    }
+}
---
+++ sample/build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.moondal.apputilsample"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.moondal.apputilsample"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+}
+
+dependencies {
+    implementation(project(":appUtilLib"))
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
